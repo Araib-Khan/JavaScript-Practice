73ca95d9e4b26d18de112e6c7e85784065cf7348
Araib-Khan <maraibniazi@gmail.com>
2022-11-02 05:31:14 -0700

    Work Done

---
+++ script.js
@@ -0,0 +1,161 @@
+// Chapter Number 1
+
+alert("Thank you for the input!");
+window.alert("Muhammad Araib Khan");
+
+// Chapter Number 2
+
+var name="Niazi";
+alert(name);
+var nationality="Pakistani";
+alert(nationality);
+var floogle = "One";
+alert(floogle);
+var lessonAuthor = "Two";
+alert(lessonAuthor);
+var OwnName = "Three";
+alert(OwnName);
+
+
+var x = "Bahria";
+alert(x);
+var lastName = "University";
+alert(lastName);
+var cityOfOrigin = "New Orleans";
+alert(cityOfOrigin);
+var aussieGreeting = "G'Day";
+alert(aussieGreeting);
+var nickname = "Bub";
+alert(nickname);
+var thanx = "Done"
+alert(thanx);
+
+// Chapter number 3
+
+var weight = 880+69;
+alert(weight);
+
+var originalNum = 13;
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNum = 27;
+var numToBeAdded = 3;
+var newNum = originalNum + numToBeAdded;
+alert(newNum);
+
+var originalNum = 80;
+originalNum = originalNum + 20;
+alert(originalNum);
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+alert(newNum);
+
+//var 1stPresident = "Washington"; illegal declaration.
+var prezWhoCame1st = "Washington"; //legal declaration.
+alert(prezWhoCame1st);
+
+alert(144);
+var caseQty = 144;
+alert(caseQty)
+
+// Chapter number 5
+
+var popularNumber = 8;
+alert(popularNumber);
+
+var popularNumber = 4 + 4;
+alert(popularNumber);
+
+alert(4 + 4);
+
+var popularNumber = 12 - 24;
+alert(popularNumber);
+
+var popularNumber = 3 * 12;
+alert(popularNumber);
+
+var num = 10;
+var popularNumber = num + 200;
+alert(popularNumber);
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+alert(popularNumber);
+
+var whatsLeftOver = 10 % 3;
+alert(whatsLeftOver);
+
+var whatsLeftOver = 9 % 3;
+alert(whatsLeftOver);
+
+// Chapter number 6
+
+var num=10;
+num = num + 1;
+num++;
+num--;
+alert(num);
+
+var num = 1;
+var newNum = num++;
+alert(newNum);
+
+var num = 1;
+var newNum = ++num;
+alert(newNum);
+
+var num = 1;
+var newNum = num--;
+alert(newNum);
+
+var num = 1;
+var newNum = --num;
+alert(newNum);
+
+// Chapter number 7
+
+var totalCost = 1 + 3 * 4;
+alert(totalCost);
+
+var totalCost = 1 + (3 * 4);
+alert(totalCost);
+
+var totalCost = (1 + 3) * 4;
+alert(totalCost);
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+alert(resultOfComputation);
+
+resultOfComputation = ((2 * 4) * 4) + 2;
+alert(resultOfComputation);
+
+resultOfComputation = (2 * 4) * (4 + 2);
+alert(resultOfComputation);
+
+// Chapter number 8
+
+var message = "Thanks for your input!";
+alert(message);
+
+var userName= "Muhammad Araib Khan";
+alert("Thanks, " + userName + "!");
+
+var message = "Thanks, ";
+userName = "Araib";
+var bang = "!";
+alert(message + userName + bang);
+
+alert("Thanks, " + "Araib" + "!");
+
+var message = "Thanks, ";
+var userName = "Ak Niazi";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+
+alert("5" + "5"); 
+
+alert("5 plus 5 equals :" + 5 + 5);
